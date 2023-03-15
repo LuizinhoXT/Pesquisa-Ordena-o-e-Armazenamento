98e30bdc1626389bc3d46b481fb1d18c391c17ad
LuizinhoXT <luiz.farias.work@outlook.com>
2023-03-15 00:06:23 -0300

    Adicionando Projeto_2_Hash_Map (To_do) Adicionando Projeto_3_Algoritmoos_de_Busca

---
+++ Projeto_3_Algoritmos_de_Busca/lib/Busca.java
@@ -0,0 +1,36 @@
+package Projeto_3_Algoritmos_de_Busca.lib;
+
+
+// A busca binária começa analisando o elemento do meio da lista/vetor.
+// se a chave de busca for igual ao elemento do meio, então, retornamos o elemento.
+// se não, iremos ignorar um dos lados e realizar a busca 
+// para tal, faz: se chave de busca for maior que elemento do meio, então ignoramos o lado esquerdo do vetor e trabalhamos só com o direito 
+public class Busca {
+
+    public int binária(int[] vetor, int chaveDeBusca){
+        
+        int indice = -1;
+
+        int inicio = 0;
+        int fim = (vetor.length - 1);
+        
+        
+        while (inicio <= fim){
+            int meio =((inicio + fim)/2);
+            if(chaveDeBusca == vetor[meio]){
+                indice = meio;
+                break;
+            }else if(chaveDeBusca > vetor[meio]){
+                inicio = meio + 1;
+                
+            }else if(chaveDeBusca < vetor[meio]){
+                fim = meio - 1;
+                          
+            }
+
+        }
+
+        return indice;
+    }
+    
+}
